6b6df2bd9f9c35d433e755fd60f2194e09627e90
Ramenskii Kirill <ramenskii-kr@mail.ru>
2022-10-14 15:16:24 +0700

    CR-1
    Setting project.

---
+++ Boosty_Calculation/Application/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  Boosty_Calculation
+//
+//  Created by Кирилл Раменский on 14.10.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
